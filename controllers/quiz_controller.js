fcff22f826c88bd4da099db7f8a61e2b4a8da5df
Agomezdelamo <agomezdelamo@gmail.com>
2015-08-14 13:28:13 +0200

    "autoload"

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,30 +1,41 @@
 ﻿var models = require('../models/models.js');
 
-// GET  /quizes/index
-exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});
-	})
+// Autoload - factoriza el código si ruta incluye :quizId
+/*vamos, que si la peticion incluye una variable en la url :quizId lo pasa al objeto request como parametro para que guarde el objeto en una propiedad */
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(function(quiz){
+		if(quiz) {
+			console.log("esto es lo que se graba en el request",quiz);
+			req.quiz = quiz;
+			next();
+		}
+		else {
+			next(new Error('no existe quizId' + quizId));
+		}
+	}).catch(function(error) {next(error);});
 };
 
-// GET  /quizes/question
+// GET  /quizes/index
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(
+	function(quizes){
+		res.render('quizes/index', {quizes: quizes});
+	}).catch(function(error) {next(error);})
+};
+
+// GET  /quizes/:id
 exports.show = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
-	})
+	res.render('quizes/show', {quiz: req.quiz});
 };
 
 
 // GET  /quizes/answer
 exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta) {
-			res.render('quizes/answer', { quiz: quiz , respuesta: 'Respuesta Correcta' });
+		var resultado = "Incorrecto";
+		if(req.query.respuesta === req.quiz.respuesta) {
+			resultado = "correcto";
 		}
-		else {
-			res.render('quizes/answer', { quiz: quizORMtoParam, respuesta: 'Respuesta Incorrecta'});
-		}
-	})
+		res.render('quizes/answer', { quiz: req.quiz , respuesta: resultado });
 };
 
 
