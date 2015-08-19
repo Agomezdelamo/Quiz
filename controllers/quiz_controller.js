521ccb43bd7e726290bccf4ffa3897722f515bcb
Agomezdelamo <agomezdelamo@gmail.com>
2015-08-19 18:59:08 +0200

    "validar, editar y borrar preguntas"

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -5,6 +5,7 @@
 exports.load = function(req, res, next, quizId) {
 	models.Quiz.find(quizId).then(function(quiz){
 		if(quiz) {
+			//autoload realiza la búsqueda en la tabla antes de invocar el controlador, dejando el objeto en req.quiz
 			console.log("esto es lo que se graba en el request",quiz);
 			req.quiz = quiz;
 			next();
@@ -19,11 +20,11 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(
 		function(quizes){
-			res.render('quizes/index', {quizes: quizes});
+			res.render('quizes/index', {quizes: quizes, errors: []});
 		}).catch(function(error) {next(error);})
 };
 
-// GET  /quizes/index
+// GET  /quizes/
 exports.search = function(req, res, next) {
 	var search = req.query.search;
 	var sequelizado = "%"+search.replace(" ", "%")+"%";
@@ -49,7 +50,7 @@ exports.answer = function(req, res) {
 		if(req.query.respuesta === req.quiz.respuesta) {
 			resultado = "correcto";
 		}
-		res.render('quizes/answer', { quiz: req.quiz , respuesta: resultado });
+		res.render('quizes/answer', { quiz: req.quiz , respuesta: resultado, errors: [] });
 };
 
 // GET /quizes/new
@@ -57,7 +58,7 @@ exports.new = function(req,res) {
 	var quiz = models.Quiz.build(//crea un objeto quiz solo para renderizar vistas, no es persistente, no se queda en base de datos.
 	{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
@@ -67,7 +68,61 @@ exports.create = function(req,res) {
 	//que están accesibles en req.body.quiz.
 	var quiz = models.Quiz.build(req.body.quiz);
 	//y lo guardo en DB, tanto pregunta como respuesta.
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+	quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		}
+		else{
+			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 			res.redirect('/quizes');
-	})	//redirección http (url relativo) lista de preguntas PORQUE NO TENGO VISTA ASOCIADA.
+			//redirección http (url relativo) lista de preguntas PORQUE NO TENGO VISTA ASOCIADA.
+			})
+		}
+	})	
+};
+
+
+// POST /quizes/:id/edit
+exports.edit = function(req,res) {
+	var quiz = req.quiz; //autoload de la instacia quiz.
+	res.render('quizes/edit', {quiz: quiz, errors: []})
+};
+
+// PUT /quizes/:id
+exports.update = function(req,res) {
+	/* actualiza la DB con la pregunta corregida.
+	Cuando damos a salvar la pregunta editada, invocamos la ruta quizes/con el id, en método http put, que es para modificar una tabla.
+	Eso viene dado por el actión del formulario.
+	Además, al llamar a esta ruta, el autoload guarda el objeto requerido(el modelo orm de la tabla), en req.quiz, y después llama al controlador, update.*/
+	
+	
+	/*El controlador actualiza las propiedades pregunta y respuesta de req.quiz con lo enviado desde el formulario*/
+	req.quiz.pregunta = req.body.quiz.pregunta; //actualizamos
+	//objeto para db ---- valor enviado por formulario.
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz.validate()//validamos
+	.then(
+	function(err){
+		if(err){
+			res.render('quizes/edit', {quiz:req.quiz, errors: err.errors});
+		}
+		
+		/* actualiza la DB y
+		edirecciona a la lista de preguntas con la pregunta ya corregida.*/
+		else {
+			req.quiz	//salvamos los campos en DB.
+			.save( {fields: ["pregunta","respuesta"]})
+			.then( function() {res.redirect('/quizes');});
+				//redireccionamos a preguntas con la pregunta modificada.
+		}
+	});
+};
+
+// POST /quizes/:id/edit
+exports.destroy = function(req,res) {
+	req.quiz.destroy().then(
+		function(){
+			res.redirect('/quizes');
+		}).catch(function(error){next(error)});
 };
