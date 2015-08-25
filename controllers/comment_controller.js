84a8a5b9fc1e7b3247a5ce6b952c90b811e3444c
Agomezdelamo <agomezdelamo@gmail.com>
2015-08-25 16:38:16 +0200

    "moderación de comentarios"

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,6 +1,23 @@
 ﻿//Importo el modelo
 var models = require('../models/models.js');
 
+
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId) {
+  models.Comment.find({
+            where: {
+                id: Number(commentId)
+            }
+        }).then(function(comment) {
+      if (comment) {
+        req.comment = comment;
+        next();
+      } else{next(new Error('No existe commentId=' + commentId))}
+    }
+  ).catch(function(error){next(error)});
+};
+
+
 // GET /comments/new   CREAR COMENTARIO FRONT
 exports.new = function(req,res) {
 	res.render('comments/new.ejs', {quizId: req.params.quizId, errors: []});
@@ -32,3 +49,13 @@ Se utiliza el nombre :quizId definido en la ruta en routes/index.js.*/
 	}).catch(function(error){next(error)});	
 };
 
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+  req.comment.publicado = true;
+
+  req.comment.save( {fields: ["publicado"]})
+    .then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
+    .catch(function(error){next(error)});
+
+};
